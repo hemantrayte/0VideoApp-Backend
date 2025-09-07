673a87c8eda7135aba626416c0e486818d70bfe5
hemant <hemantrayte25@gmail.com>
2025-09-07 15:56:29 +0530

    refresh token and acees token generatane and login user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,22 @@ import {User} from '../models/user.model.js'
 import {uploadOnCloudinary} from "../utils/cloundinary.js"
 import {ApiResponse} from '../utils/ApiResponse.js'
 
+const generateAccessAndRefresTokens = async(userId) => {
+  try {
+    const user = await User.findById(userId)
+   const accessToken = user.generateAccessToken()
+   const refreshToken = user.generateRefreshToken()
+
+    user.refreshToken =refreshToken
+    await user.save({validateBeforeSave: false})
+
+    return {accessToken, refreshToken}
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong while generating refresh and access token")
+  }
+}
+
+
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from frontend
   //validation - not empty
@@ -62,7 +78,7 @@ if (!avatarLocalPath) {
   }
 
   //step 6 -create user object - create entry in db
-  
+
   const user = await User.create({
     fullName,
     avatar:avatar.url,
@@ -88,4 +104,69 @@ if (!avatarLocalPath) {
   )
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async (req, res) => {
+    // red body -> data
+    //username or email if user exist
+    //find the user
+    //password cheak
+    //access and refresh token
+    //send cookies
+
+    //step - req body -> data
+    const {email, username, password} = req.body
+
+    if(!username || !email) {
+      throw new ApiError(400, "username or email is required")
+    }
+
+    //step 2 -username or email if user exist find the user
+    const user = await User.findOne({
+      $or:[{username}, {email}]
+    })
+
+    if (!user) {
+      throw new ApiError(404, "User does not exist")
+    }
+
+    //step 3-comapre the password cheak password
+    const isPasswordValid =  await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+      throw new ApiError(401,"Invaild user credentials")
+    }
+
+    //step 4- generate access and refresh token
+    const {accessToken, refreshToken} = await generateAccessAndRefresTokens(user._id)
+
+    //feilds are not send to responce
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+    
+    //step 5 -send cookies(refresh token and acees token)
+    const options = {
+      httpOnly:true,
+      secure:true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser, accessToken, refreshToken
+        },
+        "Userlogged In Successfully"
+      )
+    )
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+   
+})
+
+export {
+  registerUser,
+  loginUser
+}
