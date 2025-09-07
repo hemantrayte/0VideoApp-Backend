bcc5bf8c3bd9d7f822db5a35bb6561b1ff3c7562
hemant <hemantrayte25@gmail.com>
2025-09-07 16:32:29 +0530

    logout controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -163,7 +163,28 @@ const loginUser = asyncHandler(async (req, res) => {
 })
 
 const logoutUser = asyncHandler(async (req, res) => {
-   
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set:{
+        refreshToken:undefined
+      }
+    },
+    {
+      new: true
+    }
+  )
+
+  const options = {
+    httpOnly:true,
+    secure:true
+  }
+
+  return res
+  .status(200)
+  .clearCookies("accessToken", options)
+  .clearCookies("refreshToken", options)
+  .json(new ApiResponse(200, {}, "User logged Out"))
 })
 
 export {
