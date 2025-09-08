c2e4344b24b68caf2dec0e7390abcc654158837b
hemant <hemantrayte25@gmail.com>
2025-09-08 13:07:50 +0530

    fix: correct ApiResponse instantiation in changeCurrentPassword function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -245,7 +245,7 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
 
   return res
   .status(200)
-  .json(new ApiResponse, {},"Password changed successfully")
+  .json(new ApiResponse(200, {},"Password changed successfully"))
 })
 
 const getCurrentUser = asyncHandler(async(req, res) => {
@@ -352,4 +352,5 @@ export {
   getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
+  updateUserCoverImage,
 }
