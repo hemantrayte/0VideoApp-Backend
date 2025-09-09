331fff7d7a3c786a88d29b9c2a4a93e784a3d39b
hemant <hemantrayte25@gmail.com>
2025-09-09 17:01:34 +0530

    add: implement healthcheck controller to monitor server status and uptime

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -75,7 +75,6 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
 })
 
 // controller to return channel list to which user has subscribed
-// controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
 
