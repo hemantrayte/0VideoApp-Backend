958d2638cd711601a324f4d29100cb1441c8f1cd
hemant <hemantrayte25@gmail.com>
2025-09-09 16:57:56 +0530

    implement functions to fetch channel subscribers and subscribed channels with error handling

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -4,7 +4,7 @@ import { Subscription } from "../models/subscription.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
-import { subscribe } from "diagnostics_channel"
+import { channel, subscribe } from "diagnostics_channel"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
@@ -52,12 +52,54 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    if (!channelId) {
+      throw new ApiError(400, "Channel ID is required");
+    }
+
+    const subscribers = await Subscription.find({ channel: channelId })
+    .populate("subscriber", "fullName username avatar") // show subscriber details
+    .sort({ createdAt: -1 }); // newest subscribers first
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        totalSubscribers: subscribers.length,
+        subscribers,
+      },
+      "Channel subscribers fetched successfully"
+    )
+  );
+
 })
 
 // controller to return channel list to which user has subscribed
+// controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
-})
+  const { subscriberId } = req.params;
+
+  if (!subscriberId) {
+    throw new ApiError(400, "Subscriber ID is required");
+  }
+
+  // find all subscriptions where the given user is the subscriber
+  const channelList = await Subscription.find({ subscriber: subscriberId })
+    .populate("channel", "fullName username avatar") // populate channel info
+    .sort({ createdAt: -1 });
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        totalChannels: channelList.length,
+        channels: channelList.map(sub => sub.channel), // just channel details
+      },
+      "Subscribed channels fetched successfully"
+    )
+  );
+});
+
 
 export {
     toggleSubscription,
