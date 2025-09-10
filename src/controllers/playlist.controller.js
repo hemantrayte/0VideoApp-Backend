da219a09d322a4e04a70af00141477fc3c6ffff1
hemant <hemantrayte25@gmail.com>
2025-09-10 08:52:31 +0530

    add: implement playlist controller with functions to create, retrieve, update, and delete playlists

---
+++ src/controllers/playlist.controller.js
@@ -0,0 +1,68 @@
+import mongoose, {isValidObjectId} from "mongoose"
+import {Playlist} from "../models/playlist.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+
+const createPlaylist = asyncHandler(async (req, res) => {
+    const {name, description} = req.body
+    const userId = req.user?._id; // from auth middleware
+    //TODO: create playlist
+
+    if (!name || !description) {
+      throw new ApiError(400, "Name and description are required");
+    }
+
+    const playList = await Playlist.create({
+      name:name,
+      description:description,
+      owner:userId,
+      videos: [] // initially empty
+    })
+
+    return res.status(201).json(
+      new ApiResponse(201, playList, "Playlist created successfully")
+    );
+})
+
+const getUserPlaylists = asyncHandler(async (req, res) => {
+    const {userId} = req.params
+    //TODO: get user playlists
+})
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    //TODO: get playlist by id
+})
+
+const addVideoToPlaylist = asyncHandler(async (req, res) => {
+    const {playlistId, videoId} = req.params
+})
+
+const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
+    const {playlistId, videoId} = req.params
+    // TODO: remove video from playlist
+
+})
+
+const deletePlaylist = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    // TODO: delete playlist
+})
+
+const updatePlaylist = asyncHandler(async (req, res) => {
+    const {playlistId} = req.params
+    const {name, description} = req.body
+    //TODO: update playlist
+})
+
+export {
+    createPlaylist,
+    getUserPlaylists,
+    getPlaylistById,
+    addVideoToPlaylist,
+    removeVideoFromPlaylist,
+    deletePlaylist,
+    updatePlaylist
+}
