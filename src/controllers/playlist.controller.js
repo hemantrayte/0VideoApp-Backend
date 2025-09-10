7e1d89515df4456ad18063cbd55186abdd8744f0
hemant <hemantrayte25@gmail.com>
2025-09-10 09:10:34 +0530

    add: implement error handling for fetching user playlists and playlist by ID

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -29,15 +29,53 @@ const createPlaylist = asyncHandler(async (req, res) => {
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
     //TODO: get user playlists
+
+    if (!userId) {
+      throw new ApiError(400, "User ID is required");
+    }
+
+    const userPlaylists = await Playlist.find({ owner: userId });
+
+    if (userPlaylists.length === 0) {
+      throw new ApiError(404, "No playlists found for this user");
+    }
+
+    return res.status(200).json(
+      new ApiResponse(
+        200,
+        userPlaylists,
+        "User playlists fetched successfully"
+      )
+    );
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     //TODO: get playlist by id
+
+    if(!playlistId) {
+      throw new ApiError(400, "Playlist ID is required")
+    }
+
+    const playlistById = await Playlist.findById(playlistId).populate("videos");
+
+   if (!playlistById) {
+    throw new ApiError(404, "Playlist not found")
+   }
+
+   return res.status(200).json(
+    new ApiResponse(
+      200,
+      playlistById,
+      "playlist fetched successfully"
+    )
+   )
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
+    const userId = req.user?._id; // from auth middleware
+
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
