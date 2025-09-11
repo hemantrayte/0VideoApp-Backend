c4f606044a3cbd919ffdfdd83e19161d42060391
hemant <hemantrayte25@gmail.com>
2025-09-11 08:19:17 +0530

    refactor: streamline playlist deletion logic with combined validation and direct deletion

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -132,31 +132,55 @@ const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, updated, "Video removed from playlist"));
 });
 
+// const deletePlaylist = asyncHandler(async (req, res) => {
+//   const { playlistId } = req.params;
+//   const userId = req.user?._id;
+
+//   if (!playlistId) {
+//     throw new ApiError(400, "Playlist ID is required");
+//   }
+
+//   if (!userId) {
+//     throw new ApiError(400, "User ID is required");
+//   }
+
+//   // Find playlist owned by this user
+//   const playlist = await Playlist.findOne({ _id: playlistId, owner: userId });
+
+//   if (!playlist) {
+//     throw new ApiError(404, "Playlist not found or not owned by user");
+//   }
+
+//   await playlist.deleteOne();
+
+//   return res
+//     .status(200)
+//     .json(new ApiResponse(200, {}, "Playlist deleted successfully"));
+// });
+
 const deletePlaylist = asyncHandler(async (req, res) => {
   const { playlistId } = req.params;
   const userId = req.user?._id;
 
-  if (!playlistId) {
-    throw new ApiError(400, "Playlist ID is required");
+  if (!playlistId || !userId) {
+    throw new ApiError(400, "Playlist ID and User ID are required");
   }
 
-  if (!userId) {
-    throw new ApiError(400, "User ID is required");
-  }
-
-  // Find playlist owned by this user
-  const playlist = await Playlist.findOne({ _id: playlistId, owner: userId });
+  // Delete directly if found & owned by user
+  const playlist = await Playlist.findOneAndDelete({
+    _id: playlistId,
+    owner: userId,
+  });
 
   if (!playlist) {
     throw new ApiError(404, "Playlist not found or not owned by user");
   }
 
-  await playlist.deleteOne();
-
   return res
     .status(200)
     .json(new ApiResponse(200, {}, "Playlist deleted successfully"));
 });
+
 
 const updatePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
