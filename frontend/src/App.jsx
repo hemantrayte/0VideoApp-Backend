d39e6a9eebe6116f1c608da14ad5a61edbcc4a40
hemant <hemantrayte25@gmail.com>
2025-09-13 13:03:50 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
 import { Outlet } from 'react-router-dom'
-import Token from './pages/user/Token'
 
 function App() {
 
@@ -10,7 +9,6 @@ function App() {
      <Header />
      <Outlet />
      <Footer />
-     <Token />
     </>
   )
 }
