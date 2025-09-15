a88e39c66c5ddbc0dc8ea541ddc5f8053e85e56f
hemant <hemantrayte25@gmail.com>
2025-09-15 13:45:51 +0530

    Tweet Like complete

---
+++ frontend/src/pages/like/TweetLike.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import api from '../../Api/api'
+import { useParams } from 'react-router-dom'
+
+const TweetLike = () => {
+
+  const {id} = useParams()
+  const [message, setMessage] = useState(null)
+
+  const handleTweetLike = async() => {
+    try {
+      const response = await api.post(`/likes/toggle/t/${id}`)
+      console.log(response.data)
+      setMessage(response.data.message)
+    } catch (error) {
+      console.log(error.response.data)
+      setMessage(error.response.data)
+    }
+  }
+
+
+  return (
+
+    <div>
+      <button
+      onClick={handleTweetLike}
+      >Like Tweet</button>
+    </div>
+  )
+}
+
+export default TweetLike
