dd55eb4d5ea490b416136613ad72e71e8dbb14ff
hemant <hemantrayte25@gmail.com>
2025-09-15 13:50:08 +0530

    delete comments

--- frontend/src/pages/comments/DeleteComment.jsx
+++ frontend/src/pages/comments/DeleteComment.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import api from '../../Api/api';
+import { useParams } from 'react-router-dom';
 
 const DeleteComment = () => {
+ 
+  const {id} = useParams()
+
+  const handleDelete = async () => {
+    try {
+     const response = await api.delete(`/comments/c/${id}`);
+     console.log(response.data)
+    } catch (error) {
+      console.log(error.response?.data || error.message);
+    }
+  };
   return (
     <div>
-      
+      <button
+      onClick={handleDelete}
+      >Delete</button>
     </div>
   )
 }
