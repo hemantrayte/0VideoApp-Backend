ea342e3dab33d1a7f7e6b929b17cea8b12482322
hemant <hemantrayte25@gmail.com>
2025-09-15 13:46:33 +0530

    .

--- frontend/src/pages/like/TweetLike.jsx
+++ frontend/src/pages/like/TweetLike.jsx
@@ -5,16 +5,16 @@ import { useParams } from 'react-router-dom'
 const TweetLike = () => {
 
   const {id} = useParams()
-  const [message, setMessage] = useState(null)
+  // const [message, setMessage] = useState(null)
 
   const handleTweetLike = async() => {
     try {
       const response = await api.post(`/likes/toggle/t/${id}`)
       console.log(response.data)
-      setMessage(response.data.message)
+      // setMessage(response.data.message)
     } catch (error) {
       console.log(error.response.data)
-      setMessage(error.response.data)
+      // setMessage(error.response.data)
     }
   }
 
