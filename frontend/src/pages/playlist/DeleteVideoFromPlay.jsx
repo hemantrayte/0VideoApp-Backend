7eccda8e568c3e226e23dcd418fe5d11c91f32ee
hemant <hemantrayte25@gmail.com>
2025-09-16 14:23:56 +0530

    delete video from playlist

--- frontend/src/pages/playlist/DeleteVideoFromPlay.jsx
+++ frontend/src/pages/playlist/DeleteVideoFromPlay.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import api from '../../Api/api'
+import { useParams } from 'react-router-dom'
 
 const DeleteVideoFromPlay = () => {
+   
+const {playlistID, videoID} = useParams()
+
+  const handleClick =async() =>{
+    try {
+      const response = await api.delete(`/playlist/remove/${playlistID}/${videoID}`)
+      console.log(response.data)
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
+  
+  
   return (
-    <div>
-      
+    <div
+    onClick={handleClick}>
+      <button>Delete Video From Playlist </button>
     </div>
   )
 }
