5e0ec01d0cf8007202fced354fdcb08532ffc2fa
hemant <hemantrayte25@gmail.com>
2025-09-16 14:19:22 +0530

    add video to playlist

--- frontend/src/pages/playlist/AddVideoToPlaylist.jsx
+++ frontend/src/pages/playlist/AddVideoToPlaylist.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import api from '../../Api/api';
+import { useParams } from 'react-router-dom';
 
 const AddVideoToPlaylist = () => {
+
+    const [message, setMessage] = useState("");
+    const {playlistID, videoID} = useParams()
+
+ const  addVideoPlaylist = async() => {
+  try {
+    const response = await api.patch(`/playlist/add/${playlistID}/${videoID}`)
+
+    console.log(response.data)
+    setMessage(response.data.message)
+  } catch (error) {
+    setMessage(
+      error.response?.data?.message || "Something went wrong. Please try again"
+    );
+  }
+ }
+
+
   return (
     <div>
-      
+      <button
+      onClick={addVideoPlaylist}
+      >Add Video to Playlist</button>
     </div>
   )
 }
