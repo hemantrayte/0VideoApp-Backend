81c5e0e6c6330e0378a6a04f362e909e34c3232f
hemant <hemantrayte25@gmail.com>
2025-09-16 12:29:53 +0530

    delete playlist

--- frontend/src/pages/playlist/DeletePlaylist.jsx
+++ frontend/src/pages/playlist/DeletePlaylist.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import api from '../../Api/api'
 
 const DeletePlaylist = () => {
+
+  const navigate = useNavigate()
+  const {id} = useParams()
+  
+  const deletePlaylist = async() => {
+    try {
+      const response = await api.delete(`/playlist/${id}`)
+      console.log(response.data)
+      navigate(-1)
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
+  
   return (
     <div>
-      
+      <h1>real delete this playlist</h1>
+      <button
+      onClick={deletePlaylist}
+      >Delete Playlist</button>
     </div>
   )
 }
