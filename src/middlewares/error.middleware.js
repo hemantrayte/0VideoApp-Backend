a2efe030efe8d8a2afc8f0379b5d6cfd525e3a3d
hemant <hemantrayte25@gmail.com>
2026-01-22 21:31:36 +0530

    .

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -7,6 +7,7 @@ export const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
 
+  
   res.status(statusCode).json({
     statusCode,
     success: false,
