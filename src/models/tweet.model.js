ba6c7ac72b867f95362df25d770ed4f271cb9801
hemant <hemantrayte25@gmail.com>
2026-02-04 09:20:17 +0530

    adding comments to tweet model

--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -1,17 +1,57 @@
+// Import mongoose and Schema constructor
 import mongoose, { Schema } from "mongoose";
 
+/**
+ * Tweet Schema
+ * ------------
+ * Represents a short text post created by a user
+ */
 const tweetSchema = new Schema(
   {
+    /**
+     * Content of the tweet
+     */
     content: {
       type: String,
       required: true,
     },
+
+    /**
+     * Reference to the user who created the tweet
+     */
     owner: {
       type: Schema.Types.ObjectId,
       ref: "User",
     },
   },
-  { timestamps: true }
+  {
+    /**
+     * Automatically adds:
+     * - createdAt
+     * - updatedAt
+     */
+    timestamps: true,
+  }
 );
 
+// Export Tweet model
 export const Tweet = mongoose.model("Tweet", tweetSchema);
+
+
+// import mongoose, { Schema } from "mongoose";
+
+// const tweetSchema = new Schema(
+//   {
+//     content: {
+//       type: String,
+//       required: true,
+//     },
+//     owner: {
+//       type: Schema.Types.ObjectId,
+//       ref: "User",
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// export const Tweet = mongoose.model("Tweet", tweetSchema);
