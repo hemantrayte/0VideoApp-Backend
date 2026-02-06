472dc5153d08f19908e227e04f7795e7ed5da505
hemant <hemantrayte25@gmail.com>
2026-02-06 21:04:22 +0530

    add comments to user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,17 +1,30 @@
+// Import required packages
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+/**
+ * User Schema
+ * -----------
+ * Represents application users and handles authentication-related logic
+ */
 const userSchema = new mongoose.Schema(
   {
+    /**
+     * Unique username for login & identification
+     */
     username: {
       type: String,
       required: true,
       unique: true,
       trim: true,
       lowercase: true,
-      index: true, //for seraching
+      index: true, // Improves search performance
     },
+
+    /**
+     * User email address
+     */
     email: {
       type: String,
       required: true,
@@ -19,49 +32,90 @@ const userSchema = new mongoose.Schema(
       lowercase: true,
       trim: true,
     },
+
+    /**
+     * User's full name
+     */
     fullName: {
       type: String,
       required: true,
       trim: true,
       index: true,
     },
+
+    /**
+     * Profile picture (Cloudinary URL)
+     */
     avatar: {
-      type: String, ///cloudinary url
+      type: String,
       required: true,
     },
+
+    /**
+     * Channel cover image (optional)
+     */
     coverImage: {
       type: String,
     },
+
+    /**
+     * Stores videos watched by the user
+     */
     watchHistory: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Video",
       },
     ],
+
+    /**
+     * Hashed password
+     */
     password: {
       type: String,
       required: [true, "Password is required"],
     },
+
+    /**
+     * Refresh token for JWT authentication
+     */
     refreshToken: {
       type: String,
     },
   },
   {
+    /**
+     * Automatically adds:
+     * - createdAt
+     * - updatedAt
+     */
     timestamps: true,
   }
 );
 
+/**
+ * Pre-save middleware
+ * -------------------
+ * Hashes password before saving the user
+ */
 userSchema.pre("save", async function (next) {
+  // Only hash password if it is modified
   if (!this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
+/**
+ * Compare plain password with hashed password
+ */
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
+/**
+ * Generate JWT access token
+ */
 userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
@@ -77,6 +131,9 @@ userSchema.methods.generateAccessToken = function () {
   );
 };
 
+/**
+ * Generate JWT refresh token
+ */
 userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
@@ -89,4 +146,99 @@ userSchema.methods.generateRefreshToken = function () {
   );
 };
 
+// Export User model
 export const User = mongoose.model("User", userSchema);
+
+
+// import mongoose from "mongoose";
+// import bcrypt from "bcrypt";
+// import jwt from "jsonwebtoken";
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     username: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       trim: true,
+//       lowercase: true,
+//       index: true, //for seraching
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       lowercase: true,
+//       trim: true,
+//     },
+//     fullName: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//       index: true,
+//     },
+//     avatar: {
+//       type: String, ///cloudinary url
+//       required: true,
+//     },
+//     coverImage: {
+//       type: String,
+//     },
+//     watchHistory: [
+//       {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "Video",
+//       },
+//     ],
+//     password: {
+//       type: String,
+//       required: [true, "Password is required"],
+//     },
+//     refreshToken: {
+//       type: String,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+
+//   this.password = await bcrypt.hash(this.password, 10);
+//   next();
+// });
+
+// userSchema.methods.isPasswordCorrect = async function (password) {
+//   return await bcrypt.compare(password, this.password);
+// };
+
+// userSchema.methods.generateAccessToken = function () {
+//   return jwt.sign(
+//     {
+//       _id: this._id,
+//       email: this.email,
+//       username: this.username,
+//       fullName: this.fullName,
+//     },
+//     process.env.ACCESS_TOKEN_SECRET,
+//     {
+//       expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+//     }
+//   );
+// };
+
+// userSchema.methods.generateRefreshToken = function () {
+//   return jwt.sign(
+//     {
+//       _id: this._id,
+//     },
+//     process.env.REFRESH_TOKEN_SECRET,
+//     {
+//       expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+//     }
+//   );
+// };
+
+// export const User = mongoose.model("User", userSchema);
