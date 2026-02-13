37f93362b19919e61a7a3d06639af78eaaeec776
hemant <hemantrayte25@gmail.com>
2026-02-13 21:21:56 +0530

    ..

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,19 +1,38 @@
+// Load environment variables from .env file
 import dotenv from "dotenv";
 dotenv.config();
+
+// Import database connection function
 import connectDB from "./db/index.js";
+
+// Import Express app instance
 import { app } from "./app.js";
 
+// Connect to MongoDB
 connectDB()
   .then(() => {
-    // app.on("error", () => {
-    //   console.log("ERROR:", error)
-    //   throw error
-    // })
-
+    // Start server only after successful DB connection
     app.listen(process.env.PORT || 5000, () => {
-      console.log(`Server is running at port: ${process.env.PORT}`);
+      console.log(`🚀 Server is running at port: ${process.env.PORT || 5000}`);
     });
   })
   .catch((err) => {
-    console.log("MONGO DB connection failed !!!", err);
+    // If DB connection fails, log error
+    console.log("❌ MongoDB connection failed !!!", err);
   });
+
+
+// import dotenv from "dotenv";
+// dotenv.config();
+// import connectDB from "./db/index.js";
+// import { app } from "./app.js";
+
+// connectDB()
+//   .then(() => {
+//     app.listen(process.env.PORT || 5000, () => {
+//       console.log(`Server is running at port: ${process.env.PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("MONGO DB connection failed !!!", err);
+//   });
