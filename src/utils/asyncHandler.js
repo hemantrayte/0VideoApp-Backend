f3df40a526723e9c5220f726b4fb726dcbad7d38
hemant <hemantrayte25@gmail.com>
2026-02-13 21:16:35 +0530

    add comments to asynchandle.js file

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,28 @@
+
+// Wrapper function to handle async errors in Express controllers
 const asyncHandler = (requestHandler) => {
+  // Return a new middleware function
   return (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+
+    // Resolve the controller function (in case it's async)
+    // If it throws an error, catch it and pass it to next()
+    Promise
+      .resolve(requestHandler(req, res, next))
+      .catch((err) => next(err));
   };
 };
 
 export { asyncHandler };
+
+
+
+// const asyncHandler = (requestHandler) => {
+//   return (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+//   };
+// };
+
+// export { asyncHandler };
 
 // +++++++++++++ try catch handler ++++++++++++++
 
