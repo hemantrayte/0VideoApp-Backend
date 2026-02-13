364dc7c359798845f1f9c0abf8b8b918f992522b
hemant <hemantrayte25@gmail.com>
2026-02-13 21:15:36 +0530

    add comments to ApiResponse.js file

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,10 +1,34 @@
+// Custom API Response class to standardize successful responses
 class ApiResponse {
-  constructor(stausCode, data, message = "Sucess") {
-    this.stausCode = stausCode;
+  constructor(
+    statusCode,                 // HTTP status code (e.g., 200, 201)
+    data,                       // Actual response data
+    message = "Success"         // Default success message
+  ) {
+    // Store HTTP status code
+    this.statusCode = statusCode;
+
+    // Data returned from controller (user, videos, comments, etc.)
     this.data = data;
+
+    // Custom or default success message
     this.message = message;
-    this.success - stausCode < 400;
+
+    // success is true if status code is less than 400
+    this.success = statusCode < 400;
   }
 }
 
 export { ApiResponse };
+
+
+// class ApiResponse {
+//   constructor(stausCode, data, message = "Sucess") {
+//     this.stausCode = stausCode;
+//     this.data = data;
+//     this.message = message;
+//     this.success - stausCode < 400;
+//   }
+// }
+
+// export { ApiResponse };
