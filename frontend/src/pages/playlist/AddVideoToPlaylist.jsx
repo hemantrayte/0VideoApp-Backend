c60af0b98065d7d2234524accc483c85c88d16df
hemant <hemantrayte25@gmail.com>
2026-02-14 11:35:30 +0530

    add comments to addvideotoplaylist page

--- frontend/src/pages/playlist/AddVideoToPlaylist.jsx
+++ frontend/src/pages/playlist/AddVideoToPlaylist.jsx
@@ -3,30 +3,82 @@ import api from "../../Api/api";
 import { useParams } from "react-router-dom";
 
 const AddVideoToPlaylist = () => {
+
+  // State to store success or error message from backend
   const [message, setMessage] = useState("");
+
+  // Extract playlistID and videoID from URL parameters
   const { playlistID, videoID } = useParams();
 
+  // Function to add a video to a playlist
   const addVideoPlaylist = async () => {
     try {
+      // Send PATCH request to backend to add video to playlist
       const response = await api.patch(
         `/playlist/add/${playlistID}/${videoID}`
       );
 
+      // Log full response for debugging
       console.log(response.data);
+
+      // Update message state with backend success message
       setMessage(response.data.message);
+
     } catch (error) {
+      // If request fails, show backend error message
+      // If backend doesn't send one, show default message
       setMessage(
         error.response?.data?.message ||
-          "Something went wrong. Please try again"
+        "Something went wrong. Please try again"
       );
     }
   };
 
   return (
     <div>
-      <button onClick={addVideoPlaylist}>Add Video to Playlist</button>
+      {/* Button triggers video add function */}
+      <button onClick={addVideoPlaylist}>
+        Add Video to Playlist
+      </button>
+
+      {/* Display message if available */}
+      {message && <p>{message}</p>}
     </div>
   );
 };
 
 export default AddVideoToPlaylist;
+
+
+// import React, { useState } from "react";
+// import api from "../../Api/api";
+// import { useParams } from "react-router-dom";
+
+// const AddVideoToPlaylist = () => {
+//   const [message, setMessage] = useState("");
+//   const { playlistID, videoID } = useParams();
+
+//   const addVideoPlaylist = async () => {
+//     try {
+//       const response = await api.patch(
+//         `/playlist/add/${playlistID}/${videoID}`
+//       );
+
+//       console.log(response.data);
+//       setMessage(response.data.message);
+//     } catch (error) {
+//       setMessage(
+//         error.response?.data?.message ||
+//           "Something went wrong. Please try again"
+//       );
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={addVideoPlaylist}>Add Video to Playlist</button>
+//     </div>
+//   );
+// };
+
+// export default AddVideoToPlaylist;
