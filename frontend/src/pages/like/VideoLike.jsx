6af7b6e8760b8c5737365ae929829d0c352add8b
hemant <hemantrayte25@gmail.com>
2026-02-14 11:33:08 +0530

    add comment to videoLike page

--- frontend/src/pages/like/VideoLike.jsx
+++ frontend/src/pages/like/VideoLike.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import api from "../../Api/api";
 
-const VideoLike = () => {
-  const handleLike = async ({ id }) => {
+const VideoLike = ({ id }) => {
+
+  const handleLike = async () => {
     try {
       const response = await api.post(`/likes/toggle/v/${id}`);
-
       console.log(response.data);
     } catch (error) {
-      console.log("Error liking video", error.response);
+      console.log("Error liking video", error.response?.data || error.message);
     }
   };
+
   return (
     <div>
       <button onClick={handleLike}>Like</button>
@@ -19,3 +20,26 @@ const VideoLike = () => {
 };
 
 export default VideoLike;
+
+
+// import React from "react";
+// import api from "../../Api/api";
+
+// const VideoLike = () => {
+//   const handleLike = async ({ id }) => {
+//     try {
+//       const response = await api.post(`/likes/toggle/v/${id}`);
+
+//       console.log(response.data);
+//     } catch (error) {
+//       console.log("Error liking video", error.response);
+//     }
+//   };
+//   return (
+//     <div>
+//       <button onClick={handleLike}>Like</button>
+//     </div>
+//   );
+// };
+
+// export default VideoLike;
