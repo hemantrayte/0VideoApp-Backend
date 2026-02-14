d934b7f7490082189ecdf8e1a707c5aa326f8b69
hemant <hemantrayte25@gmail.com>
2026-02-14 11:28:39 +0530

    add comments to commentLike page

--- frontend/src/pages/like/CommentLike.jsx
+++ frontend/src/pages/like/CommentLike.jsx
@@ -19,3 +19,28 @@ const CommentLike = ({ id }) => {
 };
 
 export default CommentLike;
+
+
+
+
+// // import React, { useState } from 'react'
+// import api from "../../Api/api";
+
+// const CommentLike = ({ id }) => {
+//   const handleLike = async () => {
+//     try {
+//       const response = await api.post(`/likes/toggle/c/${id}`);
+//       console.log(response.data);
+//     } catch (error) {
+//       console.log(error.response.data);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={handleLike}>Like</button>
+//     </div>
+//   );
+// };
+
+// export default CommentLike;
