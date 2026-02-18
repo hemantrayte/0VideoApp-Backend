64fbb6b3ea99a9252bb05ac33382b339f350fc26
hemant <hemantrayte25@gmail.com>
2026-02-18 22:35:17 +0530

    add comments to history page

--- frontend/src/pages/user/UserWatchHistory.jsx
+++ frontend/src/pages/user/UserWatchHistory.jsx
@@ -1,3 +1,31 @@
+import React from "react";
+
+// UserWatchHistory Component
+// This component will display the list of videos watched by the current user
+const UserWatchHistory = () => {
+
+  // Currently, this component returns an empty div
+  // Later, you can add:
+  // - API call to fetch watch history
+  // - Video list UI
+  // - Thumbnail, title, duration, watched date, etc.
+
+  return (
+
+    // Main container for displaying watch history
+    <div>
+
+      {/* Watch history content will be displayed here */}
+
+    </div>
+
+  );
+};
+
+export default UserWatchHistory; // Export component for use in routing and other components
+
+
+
 // import React from "react";
 
 // const UserWatchHistory = () => {
